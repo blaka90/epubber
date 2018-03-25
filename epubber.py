0dc37974026f5a91afa916bf8559b0e4a465b26d
blaka90 <blaka90@hotmail.co.uk>
2018-03-25 18:02:23 +0100

    added code files

---
+++ epubber.py
@@ -0,0 +1,21 @@
+from renamer import *
+from scraper import *
+# from mover import *
+
+
+"""
+    after it has renamed, move to its correct folder
+    command line
+    scraping the website and downloading not working unless figure a way to login properly...
+    automating the clicks might be a better option at this point(selenium?)
+"""
+
+
+def main():
+    # login()
+    renamer()
+    print "Success!"
+
+
+if __name__ == "__main__":
+    main()
